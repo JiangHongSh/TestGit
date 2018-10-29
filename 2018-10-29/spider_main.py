7d8dab68be3dfb4dc821d88874d5a7cda550ace0
JiangHongSh <41283621+JiangHongSh@users.noreply.github.com>
2018-10-29 11:28:33 +0800

    Create spider_main.py

---
+++ 2018-10-29/spider_main.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import url_manager, html_downloader, html_parser, html_outputer
+import urllib.parse
+
+class SpiderMain(object):
+    def __init__(self):
+        self.urls=url_manager.UrlManager()
+        self.downloader=html_downloader.HtmlDownloader()
+        self.parser=html_parser.HtmlParser()
+        self.outputer=html_outputer.HtmlOutputer()
+    def craw(self, root_url):
+        count=1
+        self.urls.add_new_url(root_url)
+        while self.urls.has_new_url():
+            try:
+                new_url=self.urls.get_new_url()
+                print("craw %d:%s" %(count,(urllib.parse.unquote(new_url))))
+                html_cont=self.downloader.download(new_url)
+                new_urls,new_data=self.parser.parse(new_url,html_cont)
+                self.urls.add_new_urls(new_urls)
+                self.outputer.collect_data(new_data)
+                if count == 100:
+                    break
+                count=count+1
+            except Exception as e:
+                print(e)
+                print("craw failed")
+        self.outputer.output_html()
+
+if __name__=="__main__":
+    root_url="https://baike.baidu.com/item/Scarborough%20Fair/9424588?fr=aladdin"
+    obj_spider = SpiderMain()
+    obj_spider.craw(root_url)
