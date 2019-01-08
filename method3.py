27ebd1dcd6ab187319a3c0e0e3a1d3fe40210a63
JiangHongSh <41283621+JiangHongSh@users.noreply.github.com>
2019-01-08 16:18:43 +0800

    Delete method3.py

--- method3.py
+++
@@ -1,6 +0,0 @@
-import urllib.request,http.cookiejar
-cj = http.cookiejar.CookieJar()
-opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cj))
-urllib.request.install_opener(opener)
-response = urllib.request.urlopen("http://www.baidu.com/")
-print(response.getcode())
